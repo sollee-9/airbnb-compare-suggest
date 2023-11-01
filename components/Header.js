4e20271b7e953dd3d00d46590f941cd1e2c55f89
Sol Lee <sol@Sols-MacBook-Pro.local>
2023-11-01 17:56:37 +0900

    initial

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+
+function Header() {
+   return (
+      <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5">
+         {/* Left */}
+         <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <Image
+               src="https://links.papareact.com/qd3"
+               layout="fill"
+               objectFit="contain"
+               objectPosition="left"
+            />
+         </div>
+
+         {/* Middle */}
+         <div>Middle</div>
+
+         {/* Right */}
+         <div>Right</div>
+      </header>
+   );
+}
+
+export default Header;
